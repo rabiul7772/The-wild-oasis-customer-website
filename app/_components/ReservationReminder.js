b240fbe2761240de1eef061db16428ca626448ec
rabiul7772 <rabiulakand777@gmail.com>
2025-11-12 14:53:43 +0600

    fix: switched Supabase runtime to nodejs

--- app/_components/ReservationReminder.js
+++ app/_components/ReservationReminder.js
@@ -12,7 +12,7 @@ function ReservationReminder() {
   return (
     <div className="fixed bottom-4 left-1/2 -translate-x-1/2 py-2 px-2 md:py-5 md:px-8 rounded-full bg-accent-500 text-primary-800 text-sm md:text-base font-semibold shadow-xl shadow-slate-900 flex flex-col md:flex-row gap-3 md:gap-8 items-center w-[90%] md:w-auto max-w-md md:max-w-none text-center md:text-left">
       <p className="leading-snug">
-        <span>👋</span> Don'f forget to reserve your dates <br /> from{' '}
+        <span>👋</span> Don&apos;t forget to reserve your dates <br /> from{' '}
         {format(new Date(range.from), 'MMM dd yyyy')} to{' '}
         {format(new Date(range.to), 'MMM dd yyyy')}
       </p>
